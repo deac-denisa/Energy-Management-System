f583ad0b93be419213a60e5332ff618e74d205fa
Deac Denisa Bianca <79524870+deac-denisa@users.noreply.github.com>
2023-11-19 13:39:45 +0200

    Delete target directory

--- target/classes/data.sql
+++
@@ -1,5 +0,0 @@
--- Insert an admin user with a generated UUID
-INSERT INTO admin (id, username, password) VALUES (gen_random_uuid(), 'sonia_m', '123');
-
--- Insert a client user with a generated UUID
-INSERT INTO client (id, username, password) VALUES (gen_random_uuid(), 'marian', 'mmm');
